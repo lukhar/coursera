b2826406d91949654cbd1e0af0d77c115aafcc38
lukhar <lukasz.har@gmail.com>
2015-08-01 16:40:19 +0100

    implemented tree insert and contains

--- algorithms I princeton/kdtrees/src/main/java/KdTree.java
+++ algorithms I princeton/kdtrees/src/main/java/KdTree.java
@@ -1,4 +1,19 @@
 public class KdTree {
+
+    private Node root;
+
+    private static class Node {
+        private Point2D point;
+        private boolean compareX;
+        private Node left = null;
+        private Node right = null;
+
+        public Node(Point2D point, boolean compareX) {
+            this.point = point;
+            this.compareX = compareX;
+        }
+    }
+
     public KdTree() {
     }
 
@@ -14,6 +29,40 @@ public class KdTree {
         if (point == null) {
             throw new NullPointerException();
         }
+
+        if (root == null) {
+            root = new Node(point, true);
+        }
+
+        insert(root, point);
+    }
+
+    private void insert(Node parent, Point2D point) {
+        if (parent.compareX) {
+            if (point.x() < parent.point.x()) {
+                if (parent.left == null) {
+                    parent.left = new Node(point, !parent.compareX);
+                } else {
+                    insert(parent.left, point);
+                }
+            } else if (parent.right == null) {
+                parent.right = new Node(point, !parent.compareX);
+            } else {
+                insert(parent.right, point);
+            }
+        } else {
+            if (point.y() < parent.point.y()) {
+                if (parent.left == null) {
+                    parent.left = new Node(point, !parent.compareX);
+                } else {
+                    insert(parent.left, point);
+                }
+            } else if (parent.right == null) {
+                parent.right = new Node(point, !parent.compareX);
+            } else {
+                insert(parent.right, point);
+            }
+        }
     }
 
     public boolean contains(Point2D point) {
@@ -21,7 +70,31 @@ public class KdTree {
             throw new NullPointerException();
         }
 
-        return false;
+        return contains(root, point);
+    }
+
+    private boolean contains(Node parent, Point2D point) {
+        if (parent == null) {
+            return false;
+        }
+        if (parent.point.equals(point)) {
+            return true;
+        }
+        if (parent.compareX) {
+            if (point.x() < parent.point.x()) {
+                return contains(parent.left, point);
+            }
+            else {
+                return contains(parent.right, point);
+            }
+        } else {
+            if (point.y() < parent.point.y()) {
+                return contains(parent.left, point);
+            }
+            else {
+                return contains(parent.right, point);
+            }
+        }
     }
 
     public void draw() {
--- algorithms I princeton/kdtrees/src/test/groovy/KdTreeTest.groovy
+++ algorithms I princeton/kdtrees/src/test/groovy/KdTreeTest.groovy
@@ -1,5 +1,6 @@
 import spock.lang.Specification
 
+
 class KdTreeTest extends Specification {
 
     def 'insertion of null argument raises null pointer exception'() {
@@ -44,5 +45,35 @@ class KdTreeTest extends Specification {
 
         then:
         thrown(NullPointerException)
+    }
+
+    def 'contains inserted point'() {
+        given:
+        def points = new KdTree();
+        def insertedPoint = new Point2D(4.5, 6.7)
+
+        when:
+        points.insert(insertedPoint)
+
+        then:
+        assert points.contains(insertedPoint)
+    }
+
+    def 'given multiple points inserted contains all of them'() {
+        given:
+        def points = new KdTree()
+        def a = new Point2D(-0.5, 12)
+        def b = new Point2D(8.5, -4.5)
+        def c = new Point2D(3.2, 1.5)
+
+        when:
+        points.insert(a)
+        points.insert(b)
+        points.insert(c)
+
+        then:
+        assert points.contains(a)
+        assert points.contains(b)
+        assert points.contains(c)
     }
 }
