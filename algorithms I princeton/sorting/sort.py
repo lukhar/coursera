a07af2c335d7196ff26ea1d675521cbf1dc8b647
lukhar <lukasz.har@gmail.com>
2015-09-26 14:43:27 +0100

    Extracted common buffer.

--- algorithms I princeton/sorting/sort.py
+++ algorithms I princeton/sorting/sort.py
@@ -18,67 +18,78 @@ def insertion_sort(sequence, delimeter):
     return sequence
 
 
-def merge(sequence, lo, mid, hi):
-    aux = list(sequence)
-    i, j, k = lo, mid + 1, lo
+class MergeSort(object):
 
-    while k <= hi:
-        if i > mid:
-            sequence[k] = aux[j]
-            j += 1
-        elif j > hi:
-            sequence[k] = aux[i]
-            i += 1
-        elif aux[i] < aux[j]:
-            sequence[k] = aux[i]
-            i += 1
+    _buffer = []
+
+    @classmethod
+    def merge(cls, sequence, lo, mid, hi):
+        if len(cls._buffer) != len(sequence):
+            cls._buffer = list(sequence)
         else:
-            sequence[k] = aux[j]
-            j += 1
-        k += 1
+            for i in xrange(len(sequence)):
+                cls._buffer[i] = sequence[i]
 
-    return sequence
+        i, j, k = lo, mid + 1, lo
 
-
-def top_down_mergesort(sequence, delimeter):
-
-    counter = {'merges': 0}
-
-    def sort(sequence, lo, hi):
-        if lo >= hi:
-            return sequence
-
-        mid = lo + (hi - lo) / 2
-        sort(sequence, lo, mid)
-        sort(sequence, mid + 1, hi)
-
-        if counter['merges'] == delimeter:
-            return sequence
-        else:
-            merge(sequence, lo, mid, hi)
-            counter['merges'] += 1
+        while k <= hi:
+            if i > mid:
+                sequence[k] = cls._buffer[j]
+                j += 1
+            elif j > hi:
+                sequence[k] = cls._buffer[i]
+                i += 1
+            elif cls._buffer[i] < cls._buffer[j]:
+                sequence[k] = cls._buffer[i]
+                i += 1
+            else:
+                sequence[k] = cls._buffer[j]
+                j += 1
+            k += 1
 
         return sequence
 
-    return sort(sequence, 0, len(sequence) - 1)
+    @classmethod
+    def top_down_mergesort(cls, sequence, delimeter):
+        cls._buffer = [0] * len(sequence)
 
+        counter = {'merges': 0}
 
-def bottom_up_mergesort(sequence, delimeter):
-    lo, sz = 0, 1
-    counter = 0
-
-    while sz < len(sequence):
-        lo = 0
-        while lo < len(sequence) - sz:
-            if counter == delimeter:
+        def sort(sequence, lo, hi):
+            if lo >= hi:
                 return sequence
 
-            merge(sequence, lo, lo + sz - 1, min(lo + sz + sz - 1, len(sequence) - 1))
-            counter += 1
-            lo += (sz + sz)
-        sz += 1
+            mid = lo + (hi - lo) / 2
+            sort(sequence, lo, mid)
+            sort(sequence, mid + 1, hi)
 
-    return sequence
+            if counter['merges'] == delimeter:
+                return sequence
+            else:
+                cls.merge(sequence, lo, mid, hi)
+                counter['merges'] += 1
+
+            return sequence
+
+        return sort(sequence, 0, len(sequence) - 1)
+
+    @classmethod
+    def bottom_up_mergesort(cls, sequence, delimeter):
+        cls._buffer = [0] * len(sequence)
+        lo, sz, counter = 0, 1, 0
+
+        while sz < len(sequence):
+            lo = 0
+            while lo < len(sequence) - sz:
+                if counter == delimeter:
+                    return sequence
+
+                cls.merge(sequence, lo, lo + sz - 1, min(lo + sz + sz - 1, len(sequence) - 1))
+                counter += 1
+                lo += (sz + sz)
+            sz += 1
+
+        return sequence
 
 
 def partition(sequence, lo, hi):
@@ -107,6 +118,6 @@ if __name__ == '__main__':
     import sys
     sequence = [int(val) for val in sys.argv[1:]]
     print 'insertion sort:       ', ' '.join(str(val) for val in insertion_sort(list(sequence), delimeter=4))
-    print 'top-down merge sort:  ', ' '.join(str(val) for val in top_down_mergesort(list(sequence), delimeter=7))
-    print 'bottom-up merge sort: ', ' '.join(str(val) for val in bottom_up_mergesort(list(sequence), delimeter=7))
+    print 'top-down merge sort:  ', ' '.join(str(val) for val in MergeSort.top_down_mergesort(list(sequence), delimeter=7))
+    print 'bottom-up merge sort: ', ' '.join(str(val) for val in MergeSort.bottom_up_mergesort(list(sequence), delimeter=7))
     print 'partition two-way     ', ' '.join(str(val) for val in partition(list(sequence), lo=0, hi=len(sequence) - 1))
--- algorithms I princeton/sorting/test_sort.py
+++ algorithms I princeton/sorting/test_sort.py
@@ -1,5 +1,5 @@
 from nose.tools import assert_equal
-from sort import insertion_sort, top_down_mergesort, merge, bottom_up_mergesort, partition
+from sort import partition, MergeSort, insertion_sort
 
 
 def test_given_sequnce_and_delimiter_n_return_subsorted_seq_after_n_exchanges():
@@ -7,22 +7,27 @@ def test_given_sequnce_and_delimiter_n_return_subsorted_seq_after_n_exchanges():
 
 
 def test_partially_sort_sequence_using_top_down_merge_sort():
-    assert_equal(top_down_mergesort([2, 1, 7, 4, 5, 6], delimeter=1), [1, 2, 7, 4, 5, 6])
-    assert_equal(top_down_mergesort([45, 51, 92, 25, 93, 22, 35, 27, 29, 49, 80, 19], delimeter=7),
-                 [22, 25, 45, 51, 92, 93, 27, 29, 35, 49, 80, 19])
+    assert_equal(
+        MergeSort.top_down_mergesort([2, 1, 7, 4, 5, 6], delimeter=1), [1, 2, 7, 4, 5, 6])
+    assert_equal(
+        MergeSort.top_down_mergesort([45, 51, 92, 25, 93, 22, 35, 27, 29, 49, 80, 19],
+                                     delimeter=7),
+        [22, 25, 45, 51, 92, 93, 27, 29, 35, 49, 80, 19])
 
 
 def test_partially_sort_sequence_using_bottom_up_merge_sort():
-    assert_equal(bottom_up_mergesort([76, 43, 51, 90, 68, 73, 57, 41, 99, 84], delimeter=7),
-                 [43, 51, 76, 90, 41, 57, 68, 73, 84, 99])
+    assert_equal(
+        MergeSort.bottom_up_mergesort(
+            [76, 43, 51, 90, 68, 73, 57, 41, 99, 84],
+            delimeter=7),
+        [43, 51, 76, 90, 41, 57, 68, 73, 84, 99])
 
 
 def test_merge_two_lists():
-    assert_equal(merge([1, 3, 7, 2, 5, 6], lo=0, mid=2, hi=5), [1, 2, 3, 5, 6, 7])
+    assert_equal(
+        MergeSort.merge([1, 3, 7, 2, 5, 6], lo=0, mid=2, hi=5),
+        [1, 2, 3, 5, 6, 7])
 
 
 def test_partition():
     assert_equal(partition([5, 4, 2, 11, 10], lo=0, hi=4), [2, 4, 5, 11, 10])
-    assert_equal(partition(['K', 'R', 'A', 'T', 'E', 'L', 'E', 'P', 'U', 'I', 'M', 'Q', 'C', 'X', 'O', 'S'],
-                           lo=0, hi=15),
-                 ['E', 'C', 'A', 'I', 'E', 'K', 'L', 'P', 'U', 'T', 'M', 'Q', 'R', 'X', 'O', 'S'])
